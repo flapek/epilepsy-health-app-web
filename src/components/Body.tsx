6b382b72ee3b93707fb94645cd8b48bd1e0992b1
Flapek <filap4@gmail.com>
2021-03-13 15:03:32 +0100

    Update Body.tsx

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -30,37 +30,13 @@ const routes = [
 
 export default function Body() {
     return (
-        // <Switch>
-        //   <Route exact path="/">
-        //     <Home />
-        //   </Route>
-        //   <Route path="/about">
-        //     <About />
-        //   </Route>
-        //   <Route path="/login">
-        //     <Login />
-        //   </Route>
-        //   <Route path="/register">
-        //     <Register />
-        //   </Route>
-        //   <Route path="*">
-        //     <NotFound />
-        //   </Route>
-        // </Switch> 
         <Switch>
             {routes.map((route, i)=>{
-                if (i===1)
-                    return (
-                        <Route key={i} path={route.path} exact>
-                            {route.component}
-                        </Route>
-                    );
-                else
-                    return (
-                        <Route key={i} path={route.path}>
-                            {route.component}
-                        </Route>
-                    );
+                return (
+                    <Route key={i} path={route.path} render={() => (
+                        <route.component />
+                    )}/>
+                );
             })}
         </Switch>
     );
