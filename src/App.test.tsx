dcd4f9facadd5700de82a59061972ae98c655564
Flapek <filap4@gmail.com>
2021-04-26 22:24:08 +0200

    update versions of packages

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Header from './components/Header'
-
-// test('Header render home', () => {
-//   render(<Header />);
-//   const linkElement = screen.getByText(/Home/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
