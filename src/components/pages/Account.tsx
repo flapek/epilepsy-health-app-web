b0cf04c6a0debbba393ca587b66ab74545ab3c79
Flapek <filap4@gmail.com>
2021-05-29 15:19:38 +0200

    Update Account.tsx

--- src/components/pages/Account.tsx
+++ src/components/pages/Account.tsx
@@ -57,7 +57,6 @@ export default function Account() {
                           <FormControl>
                             <InputLabel htmlFor="my-input">Email address</InputLabel>
                             <Input id="my-input" aria-describedby="my-helper-text" />
-                            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
                           </FormControl>
                         </Paper>
                     </Grid>
