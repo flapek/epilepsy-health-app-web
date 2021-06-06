779085dd3ab646e3e9b066adaeaa9fed0f421789
Flapek <filap4@gmail.com>
2021-06-07 00:17:24 +0200

    Update Account.tsx

--- src/components/pages/Account.tsx
+++ src/components/pages/Account.tsx
@@ -36,77 +36,77 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Account() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [values, setValues] = useState({
-      firstName: 'Katarina',
-      lastName: 'Smith',
-      email: 'demo@devias.io',
-      password: 'asddwqdas',
-      phone: '',
-      age: '20',
-      weight: '55',
-    });
-  
-    function handleChange(event) {
+  const [values, setValues] = useState({
+    firstName: 'Katarina',
+    lastName: 'Smith',
+    email: 'demo@devias.io',
+    password: 'asddwqdas',
+    phone: '',
+    age: '20',
+    weight: '55',
+  });
+
+  function handleChange(event) {
     setValues({
       ...values,
       [event.target.name]: event.target.value
     });
   }
 
-    return (
-        <DefaultPage>
-            <Container maxWidth="lg" className={classes.container}>
-            <form autoComplete="off" noValidate >
-                <Card>
-                  <CardHeader
-                    subheader="The information can be edited"
-                    title="Profile"
-                  />
-                  <Divider />
-                  <CardContent>
-                    <Grid container spacing={3}>
-                      <Grid item md={6} xs={12}>
-                        <TextField fullWidth helperText="Please specify the first name"
-                          label="First name" name="firstName" onChange={handleChange}
-                          value={values.firstName} variant="outlined"/>
-                      </Grid>
-                      <Grid item md={6} xs={12}>
-                        <TextField fullWidth label="Last name" name="lastName"
-                          onChange={handleChange} value={values.lastName} variant="outlined"/>
-                      </Grid>
-                      <Grid item md={6} xs={12}>
-                        <TextField fullWidth label="Email Address" name="email" 
-                          onChange={handleChange} value={values.email}
-                          variant="outlined"/>
-                      </Grid>
-                      <Grid item md={6} xs={12}>
-                        <TextField fullWidth label="Password" name="password" 
-                          onChange={handleChange} value={values.password}
-                          variant="outlined" type="password"/>
-                      </Grid>
-                      <Grid item md={6} xs={12}>
-                        <TextField fullWidth label="Age" name="age" 
-                          onChange={handleChange} value={values.age}
-                          variant="outlined"/>
-                      </Grid>
-                      <Grid item md={6} xs={12}>
-                        <TextField fullWidth label="Weight" name="weight" 
-                          onChange={handleChange} value={values.weight}
-                          variant="outlined"/>
-                      </Grid>
+  return (
+      <DefaultPage>
+          <Container maxWidth="lg" className={classes.container}>
+          <form autoComplete="off" noValidate >
+              <Card>
+                <CardHeader
+                  subheader="The information can be edited"
+                  title="Profile"
+                />
+                <Divider />
+                <CardContent>
+                  <Grid container spacing={3}>
+                    <Grid item md={6} xs={12}>
+                      <TextField fullWidth helperText="Please specify the first name"
+                        label="First name" name="firstName" onChange={handleChange}
+                        value={values.firstName} variant="outlined"/>
                     </Grid>
-                  </CardContent>
-                  <Divider />
-                  <Box>
-                    <Button color="primary" variant="contained">
-                      Save details
-                    </Button>
-                  </Box>
-                </Card>
-              </form>
-            </Container>
-        </DefaultPage>
-    );
+                    <Grid item md={6} xs={12}>
+                      <TextField fullWidth label="Last name" name="lastName"
+                        onChange={handleChange} value={values.lastName} variant="outlined"/>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                      <TextField fullWidth label="Email Address" name="email" 
+                        onChange={handleChange} value={values.email}
+                        variant="outlined"/>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                      <TextField fullWidth label="Password" name="password" 
+                        onChange={handleChange} value={values.password}
+                        variant="outlined" type="password"/>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                      <TextField fullWidth label="Age" name="age" 
+                        onChange={handleChange} value={values.age}
+                        variant="outlined"/>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                      <TextField fullWidth label="Weight" name="weight" 
+                        onChange={handleChange} value={values.weight}
+                        variant="outlined"/>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+                <Divider />
+                <Box>
+                  <Button color="primary" variant="contained">
+                    Save details
+                  </Button>
+                </Box>
+              </Card>
+            </form>
+          </Container>
+      </DefaultPage>
+  );
 }
