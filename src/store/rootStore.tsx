a1154964056a714b2510d10b7ce6a6c7bcbbdab4
Flapek <filap4@gmail.com>
2021-06-10 17:52:16 +0200

    Update rootStore.tsx

--- src/store/rootStore.tsx
+++ src/store/rootStore.tsx
@@ -1,21 +1,26 @@
 import React, { createContext } from "react";
 import LoginContext from "../features/authorization/signInContext";
 import { SignInStoreSchema } from "../features/authorization/signInType";
+import UserContext from "../features/user/userContext";
+import { UserStoreSchema } from "../features/user/userType";
 
 
 type RootStoreSchema = {
     loginStore: SignInStoreSchema;
+    userStore: UserStoreSchema;
 };
 
 export const RootStoreContext = createContext<RootStoreSchema>(null);
 
 const RootStore = (props : { children: React.ReactNode }) => {
     const loginContext = LoginContext();
+    const userContext = UserContext();
 
     return (
         <RootStoreContext.Provider
             value={{
                 loginStore: loginContext,
+                userStore: userContext,
             }}>
             {props.children}
         </RootStoreContext.Provider>
