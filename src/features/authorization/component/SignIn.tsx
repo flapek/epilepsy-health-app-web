d4d453a4285f8087ca3e73dd376ae2b1c8425af4
Flapek <filap4@gmail.com>
2021-06-10 17:52:36 +0200

    Update SignIn.tsx

--- src/features/authorization/component/SignIn.tsx
+++ src/features/authorization/component/SignIn.tsx
@@ -10,7 +10,8 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { RootStoreContext } from '../../../store/rootStore';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
